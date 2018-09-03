cab3432ebf04fd8aedb5c86a1262718be6781c36
DewGew <pierlar@hotmail.com>
2018-09-03 15:08:51 +0200

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -56,20 +56,22 @@ function checkSettingsHTML(){
 }
 
 function searchFunction() {
-  var input, filter, table, tr, td, i;
-  input = document.getElementById("searchInput");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("main-view");
-  tr = table.getElementsByTagName("table");
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
-    if (td) {
-      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-		log(tr[i]);
-      }
-    }       
-  }
+	if (document.getElementById("dashcontent") || document.getElementById("lightcontent") || document.getElementById("scenecontent")|| document.getElementById("utilitycontent") || document.getElementById("weatherwidgets") || document.getElementById("tempwidgets")){
+	  var input, filter, table, tr, td, i;
+	  input = document.getElementById("searchInput");
+	  filter = input.value.toUpperCase();
+	  table = document.getElementById("main-view");
+	  tr = table.getElementsByTagName("table");
+	  for (i = 0; i < tr.length; i++) {
+	    td = tr[i].getElementsByTagName("td")[0];
+	    if (td) {
+	      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+		tr[i].style.display = "";
+	      } else {
+		tr[i].style.display = "none";
+			log(tr[i]);
+	      }
+	    }       
+	  }
+   	}	   
 }
