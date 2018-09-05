6b8e9dd9fe6f07bae919afedeafe9c9a4a56e48e
DewGew <pierlar@hotmail.com>
2018-09-05 15:07:18 +0200

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -195,3 +195,31 @@ function locationHashChanged() {
 		
     }
 }
+
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+	var day = date.getDate();
+	var mo = date.getMonth();
+	var y = date.getFullYear();
+	
+	mo = mo + 1;
+	mo = (mo < 10) ? "0" + mo : mo;
+    day = (day < 10) ? "0" + day : day;
+	
+	textDate = y +"-"+ mo +"-" + day
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+	document.getElementById("MyDateDisplay").innerText = textDate;
+    
+    setTimeout(showTime, 1000);
+    
+}
