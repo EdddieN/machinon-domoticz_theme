6816063ca8861ba97abe213216c79e39dd8027d8
DewGew <pierlar@hotmail.com>
2018-09-12 12:25:56 +0200

    Update check_update.js

--- js/check_update.js
+++ js/check_update.js
@@ -9,7 +9,7 @@ $.ajax({url: 'https://raw.githubusercontent.com/EdddieN/machinon-domoticz_theme/
 	gitVersion = data.version
 	
 	if (theme.version !== gitVersion) {
-		newVersionText = '<i>Machinon Version '+ data.version + ' is available! <a href="https://github.com/EdddieN/machinon-domoticz_theme/tree/' + branch + '" target="_blank">Click here to download</a></i>';
+		newVersionText = '<i>Machinon Version '+ data.version + ' is available! Make a git pull or <a href="https://github.com/EdddieN/machinon-domoticz_theme/tree/' + branch + '" target="_blank">click here to download</a></i>';
 		console.log(themeName + ' - New version is availible at github');
 	}
 }
