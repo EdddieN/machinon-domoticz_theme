c68f7a590f6c23229dfd6bb3854f05702c91ca39
DewGew <pierlar@hotmail.com>
2018-10-18 07:40:47 +0200

    Update check_update.js

--- js/check_update.js
+++ js/check_update.js
@@ -1,19 +1,19 @@
 /*
 	Check source file on github for latest version
 */
-	
 var branch = 'master';
 
-$.ajax({url: 'https://raw.githubusercontent.com/EdddieN/machinon-domoticz_theme/'+ branch +'/theme.json' , async: false, dataType: 'json', success: function(data) {
-	
-	gitVersion = data.version
-	d = new Date();
-	lastSeen = d.getTime();
-	
-	if (theme.version !== gitVersion) {
-		newVersionText = '<i>Machinon Version '+ data.version + ' is available! Make a git pull or <a href="https://github.com/EdddieN/machinon-domoticz_theme/tree/' + branch + '" target="_blank">click here to download</a></i>';
-		console.log(themeName + ' - New version is availible at github');
-		notify('Machinon Version '+ data.version + ' is available!', lastSeen);
+setTimeout(update, 5000);
+
+function update(){
+	$.ajax({url: 'https://raw.githubusercontent.com/EdddieN/machinon-domoticz_theme/'+ branch +'/theme.json' , async: false, dataType: 'json', success: function(data) {
+		
+		gitVersion = data.version
+			
+		if (theme.version !== gitVersion) {
+			newVersionText = 'Machinon version '+ data.version +' '+ language.is_available +'! <a href="https://github.com/EdddieN/machinon-domoticz_theme/tree/' + branch + '" target="_blank">' + language.click_here + '</a>';
+			generate_noty('success', '<center>' + newVersionText + '</center>', false);
+		}
 	}
+	});
 }
-});
