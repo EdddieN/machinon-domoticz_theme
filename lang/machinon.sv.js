d8ddc81fbefb808924ba407a99ff972f7759e913
DewGew <pierlar@hotmail.com>
2018-10-19 09:38:26 +0200

    Update machinon.sv.js

--- lang/machinon.sv.js
+++ lang/machinon.sv.js
@@ -19,5 +19,6 @@ jQuery.timeago.settings.strings = {
   };
 language = {
   is_available: "är tillgänglig",
-  click_here: "Klicka här för att ladda ner" 
+  click_here: "Klicka här för att ladda ner",
+  update_now: "Uppdatera Nu"
 };
