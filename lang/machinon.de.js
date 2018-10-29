95120cdfe7a358875c62fe1d60cb2ec0f893a95e
DewGew <pierlar@hotmail.com>
2018-10-29 10:16:45 +0100

    Update machinon.de.js

--- lang/machinon.de.js
+++ lang/machinon.de.js
@@ -21,5 +21,9 @@ language = {
   is_available: "is available",
   click_here: "Click here to download",
   update_now: "Update Now",
-  type_to_search: "Type to search"
+  type_to_search: "Type to search",
+  domoticz_settings_saved: "Domoticz settings saved",
+  theme_settings_saved: "Theme settings saved",
+  theme_restored: "Theme restored",
+  allow_new_hardware: "Allow new hardware for 5 min"
 };
