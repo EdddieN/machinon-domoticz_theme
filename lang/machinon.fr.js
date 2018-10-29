358b2b4c0ef0bb077817f78099fed583cc7720a0
DewGew <pierlar@hotmail.com>
2018-10-29 11:00:21 +0100

    Update machinon.fr.js

--- lang/machinon.fr.js
+++ lang/machinon.fr.js
@@ -16,13 +16,13 @@ jQuery.timeago.settings.strings = {
 	years: "%d ans"
 };
 language = {
-  is_available: "is available",
-  click_here: "Click here to download",
-  update_now: "Update Now",
-  type_to_search: "Type to search",
-  domoticz_settings_saved: "Domoticz settings saved",
-  theme_settings_saved: "Theme settings saved",
-  theme_restored: "Theme restored",
-  allow_new_hardware: "Allow new hardware for 5 min",
-  is: "is"
+  is_available: "est disponible",
+  click_here: "Download",
+  update_now: "Mettre à jour maintenant",
+  type_to_search: "Rechercher / Filtrer",
+  domoticz_settings_saved: "Paramètres Domoticz enregistrés",
+  theme_settings_saved: "Paramètres du thème enregistrés",
+  theme_restored: "Thème réinitialiser",
+  allow_new_hardware: "Autoriser le nouveau matériel pendant 5 min",
+  is: "est"
 };
