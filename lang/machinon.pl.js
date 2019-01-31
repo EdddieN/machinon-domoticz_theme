fb7b9b1a45224fc3e93510bd8546324b1bf0cedc
DewGew <pierlar@hotmail.com>
2019-01-31 10:42:07 +0100

    Add files via upload

---
+++ lang/machinon.pl.js
@@ -0,0 +1,33 @@
+// Polish (Template)
+
+ jQuery.timeago.settings.strings = {
+    prefixAgo: null,
+    prefixFromNow: null,
+    suffixAgo: "temu",
+    suffixFromNow: "od teraz",
+    seconds: "%d sekund",
+    minute: "okolo minuty",
+    minutes: "%d minut",
+    hour: "okolo godziny",
+    hours: "okolo %d godzin",
+    day: "jeden dzien",
+    days: "%d dni",
+    month: "okolo miesiaca",
+    months: "%d miesiecy",
+    year: "okolo roku",
+    years: "%d lat",
+    wordSeparator: " ",
+    numbers: []
+ };
+language = {
+  is_available: "jest dostepny",
+  click_here: "Kliknij, aby pobraæ",
+  update_now: "Aktualizuj teraz",
+  type_to_search: "Szukaj",
+  domoticz_settings_saved: "Ustawienia Domoticza zostaly zapisane",
+  theme_settings_saved: "Ustawienia wygladu zostaly zapisane",
+  theme_restored: "Ustawienia wygladu przywrocone",
+  allow_new_hardware: "Zezwól na nowe urz¹dzenia przez 5 min.",
+  is: "jest",
+  timedout: "czas minal"
+};
