2a3860ffbca66c0ff9e08cc9d76772608f743e48
DewGew <pierlar@hotmail.com>
2019-02-05 12:09:09 +0100

    Update custom_page.js

--- js/custom_page.js
+++ js/custom_page.js
@@ -1,10 +1,10 @@
 //Custom page menu
 
 $('#appnavbar li:not(.clcustommenu)').remove('.dropdown');
-var mainMenu = $('#appnavbar');
+var customMenu = $('#appnavbar');
 var customPage = mainMenu.find('#customPage');
-if (mainMenu.length && customPage.length == 0 ) {
-    mainMenu.append('<li class="divider-vertical"></li><li id="customPage"><a class="lcursor"><img src="images/setup.png"><span data-i18n="' + theme.button_name + '">' + theme.button_name + '</span></a></li>');
+if (customMenu.length && customPage.length == 0 ) {
+    customMenu.append('<li class="divider-vertical"></li><li id="customPage"><a class="lcursor"><img src="images/setup.png"><span data-i18n="' + theme.button_name + '">' + theme.button_name + '</span></a></li>');
 }
 $('#customPage').click(function() {
 var htmlcontent = '';
