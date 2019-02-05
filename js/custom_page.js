dcbbb04cec0e7d807ca01760092ae7e5a232fb3b
DewGew <pierlar@hotmail.com>
2019-02-05 15:48:06 +0100

    Update custom_page.js

--- js/custom_page.js
+++ js/custom_page.js
@@ -1,8 +1,7 @@
 //Custom page menu
 
-$('#appnavbar li:not(.clcustommenu)').remove('.dropdown');
 var customMenu = $('#appnavbar');
-var customPage = mainMenu.find('#customPage');
+var customPage = customMenu.find('#customPage');
 if (customMenu.length && customPage.length == 0 ) {
     customMenu.append('<li class="divider-vertical"></li><li id="customPage"><a class="lcursor"><img src="images/setup.png"><span data-i18n="' + theme.button_name + '">' + theme.button_name + '</span></a></li>');
 }
