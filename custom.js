08924fd1ff058c37d6e0fb1a1ca80c1c38dabe34
davidlb <david@trucs.org>
2019-07-19 14:39:08 +0200

    Remove console.log

--- custom.js
+++ custom.js
@@ -138,7 +138,6 @@ $(document).ready(function() {
     
     // Close navbar if the user clicks outside of it
     (isMobile && 992 >= window.innerWidth || !isMobile && 992 >= window.innerWidth) && $(".container li").not(".dropdown").not(".dropdown-submenu").click(function() {
-        console.log(this);
         navBarInner.removeClass("slide");
     });
     (isMobile && 992 >= window.innerWidth || !isMobile && 992 >= window.innerWidth) && $("#holder").click(function() {
